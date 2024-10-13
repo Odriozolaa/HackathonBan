1ed1b8074666e8310b20d3f2a99d3599d6a31d15
Juan Pablo Tanos <jpnmj08@gmail.com>
2024-10-13 01:56:41 -0600

    dve

--- HackathonBan/src/app/app.routes.ts
+++ HackathonBan/src/app/app.routes.ts
@@ -4,6 +4,8 @@ import { LoginComponent } from './components/login/login.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { HomeComponent } from './components/home/home.component';import { AuthGuard } from './guards/auth.guard';
 import { ProductosComponent } from './productos/productos.component';
+import { RutaComponent } from './ruta/ruta.component';
+
 
 export const appRoutes: Routes = [
 { path: 'register', component: RegisterComponent },
@@ -11,4 +13,5 @@ export const appRoutes: Routes = [
 { path: 'dashboard', component: DashboardComponent },
 { path: '', component: DashboardComponent, pathMatch: 'full' }, 
 { path: 'productos', component: ProductosComponent },
+{ path: 'ruta', component: RutaComponent },
 ];
