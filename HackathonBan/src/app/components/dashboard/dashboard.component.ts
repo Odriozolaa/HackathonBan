6144afc95b750515c22cf98b81c8675f059f7b1e
Odriozolaa <odriozolaa04@gmail.com>
2024-10-12 19:08:23 -0600

    dashboard

--- HackathonBan/src/app/components/dashboard/dashboard.component.ts
+++ HackathonBan/src/app/components/dashboard/dashboard.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { LoginComponent } from "../login/login.component";
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
-  imports: [],
+  imports: [LoginComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
